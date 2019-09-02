2c0200f23914311e84d1113e7d3065aa412ad258
Dylan Mestyanek <mestyanekdylan@gmail.com>
2019-09-02 16:23:09 -0500

    stretch game complete.

--- stretch-assignment/index.js
+++ stretch-assignment/index.js
@@ -1,22 +1,38 @@
 let blocks = document.querySelectorAll('.block');
 let blocksContainer = document.querySelector('.blocks');
-
-let pink = document.querySelector('.block--pink')
-// block.forEach(block => block.addEventListener('click', function(){
-//     document.querySelector('.blocks').prepend(this);
-// }));
-
+let count = 2;
+let moveBox;
+let moveBoxDelay;
+let mouseDown;
 
 blocks.forEach(block => block.addEventListener('click', () => {
-    block.style.transition = 'all 1s';
-    blocksContainer.style.transition = 'all 1s';
-    block.style.transform = `translateY(${document.body.scrollTop - 600}px)`;
-    blocksContainer.style.transform = `translateY(121.8px)`;
+    clearTimeout(moveBoxDelay);
+    mouseDown = false;
+    block.style.transform = `translateY(-${block.offsetTop + 108}px)`;
+    blocksContainer.style.transform = `translateY(120px)`;
+    block.style.transition = "all .3s";
+    blocksContainer.style.transition = "all .3s";
+    
     setTimeout(() => {
-    blocksContainer.style.transition = 'all 0s';
         blocksContainer.prepend(block);
-        block.style.position = "static";
+        block.style.transition = "all 0s";
+        blocksContainer.style.transition = "all 0s";
         block.style.transform = `translateY(0px)`;
         blocksContainer.style.transform = `translateY(0px)`;
+    }, 500);
+}));
+
+blocks.forEach(block => block.addEventListener('mousedown', function(){
+    mouseDown = true;
+    clearInterval(moveBox);
+
+    moveBoxDelay = setTimeout(() => {
+        moveBox = setInterval(() => {
+            mouseDown ? count += 3 : count -= 3;
+            block.style.transform = `translateX(${count}px)`;
+            count <= 0 && clearInterval(moveBox);
+        }, 10);
     }, 1000);
 }));
+
+window.addEventListener('mouseup', () => mouseDown = false);
